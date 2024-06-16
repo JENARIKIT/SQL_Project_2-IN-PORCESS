348595106c44e7f75cd2be35e9b813ecd4578fff
JENARIKIT <167493856+JENARIKIT@users.noreply.github.com>
2024-06-16 19:43:11 +0700

    Update PROJECT_2.sql

--- PROJECT_2.sql
+++ PROJECT_2.sql
@@ -1,1 +1,13 @@
 -- Q1 The quantity of noodle flour sold in 2023
+
+SELECT 
+	Group_Product,
+	strftime('%Y-%m',Posting_Date) AS MonthID,
+	SUM(Quantity_Total) AS SUM_Quantity
+From Sale_Order_Batch 
+WHERE Group_Product = "NOODLE FLOUR"
+AND MonthID like '2023%'
+Group by MonthID ;
+
+----------------------------------------------------------------------------------------------------------------------------------------------
+
